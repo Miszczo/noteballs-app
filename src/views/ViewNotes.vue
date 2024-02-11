06d55d396414885acbde5be896343eea2d1f0836
MichalGlowacki <mcglowack@gmail.com>
2024-02-11 17:08:32 +0100

    Add function to add notes

--- src/views/ViewNotes.vue
+++ src/views/ViewNotes.vue
@@ -5,14 +5,22 @@
             <div class="field">
                 <div class="control">
                     <textarea
+                        v-model="newNote"
                         class="textarea"
                         placeholder="Add a new note"
+                        ref="newNoteRef"
                     />
                 </div>
             </div>
             <div class="field is-grouped is-grouped-right">
                 <div class="control">
-                    <button class="button is-link has-background-success">Add New Note</button>
+                    <button
+                        @click="addNote"
+                        :disabled="!newNote"
+                        class="button is-link has-background-success"
+                    >
+                        Add New Note
+                    </button>
                 </div>
             </div>
         </div>
@@ -20,11 +28,12 @@
 
         <div
             class="card mb-5"
-            v-for="i in 3"
+            v-for="note in notes"
+            :key="note.id"
         >
             <div class="card-content">
                 <div class="content">
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Distinctio voluptas vitae porro praesentium earum, corporis corrupti, cum blanditiis laudantium molestias adipisci, saepe deleniti esse assumenda? Commodi dolorem quisquam ad eius.
+                    {{ note.content }}
                 </div>
             </div>
             <footer class="card-footer">
@@ -43,4 +52,45 @@
             </footer>
         </div>
     </div>
+    {{ newNote }}
 </template>
+
+<script setup>
+/**
+ * imports
+ */
+
+import { ref } from 'vue';
+import { v4 as uuidv4 } from 'uuid';
+
+/**
+ * notes
+ */
+
+const newNote = ref(''),
+    notes = ref([
+        {
+            id: 'id1',
+            content: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Distinctio voluptas vitae porro praesentium earum, corporis corrupti, cum blanditiis laudantium molestias adipisci, saepe deleniti esse assumenda? Commodi dolorem quisquam ad eius.'
+        },
+        {
+            id: 'id2',
+            content: 'ium earum, corporis corrupti, cum blanditiis laudantium molestias adipisci, saepe deleniti esse assumenda?'
+        }
+    ]),
+    newNoteRef = ref(null);
+
+/**
+ * methods
+ */
+
+const addNote = () => {
+    let note = {
+        id: uuidv4(),
+        content: newNote.value,
+    }
+    notes.value.unshift(note);
+    newNote.value = '';
+    newNoteRef.value.focus();
+}
+</script>
