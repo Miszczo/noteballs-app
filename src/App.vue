e86851022e1772255427e15d077f178819ef423d
MichalGlowacki <mcglowack@gmail.com>
2024-02-11 11:22:55 +0100

    Project init, setting up routin, clearing up code

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <RouterLink to="/">Notes</RouterLink> | 
+  <RouterLink to="/stats">Stats</RouterLink>
+  <RouterView />
+</template>
+
